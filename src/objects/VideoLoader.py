8f337611cb75812587b3ac00959bfc4c5cf13fd0
Andrii Sonsiadlo <andrii.sonsiadlo@gmail.com>
2022-12-11 19:30:00 +0100

    feature: code for video classification

---
+++ src/objects/VideoLoader.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+from objects.ConfigReader import ConfigReader
+from objects.Singleton import Singleton
+
+
+class VideoLoader(metaclass=Singleton):
+
+    def load_video(self, path: str, img_resize, max_video_frames=0):
+        capture = cv2.VideoCapture(path)
+        frames = []
+        try:
+            while True:
+                ret, frame = capture.read()
+                if not ret:
+                    break
+                frame = self.crop_center_square(frame)
+                frame = cv2.resize(frame, img_resize)
+                frame = frame[:, :, [2, 1, 0]]
+                frames.append(frame)
+
+                if len(frames) == max_video_frames:
+                    break
+        finally:
+            capture.release()
+        return np.array(frames)
+
+    def crop_center_square(self, frame):
+        y, x = frame.shape[0:2]
+        min_dim = min(y, x)
+        start_x = (x // 2) - (min_dim // 2)
+        start_y = (y // 2) - (min_dim // 2)
+        return frame[start_y: start_y + min_dim, start_x: start_x + min_dim]
---
+++ src/utils/utils.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+from objects.ConfigReader import ConfigReader
+
+
+def set_gpu_config():
+    hardware_params = ConfigReader()()["hardware"]
+    VRAM = hardware_params.getint("VRAM")
+
+    gpus = tf.config.experimental.list_physical_devices("GPU")
+    if gpus:
+        try:
+            tf.config.experimental.set_virtual_device_configuration(
+                gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=VRAM)]
+            )
+        except RuntimeError as e:
+            print(e)
+
+
