bc23ea8a3a94ac1c0222c713c64f3e9bfd71788a
Andrii Sonsiadlo <andrii.sonsiadlo@gmail.com>
2022-12-21 08:57:14 +0100

    update GUI, attach functionality to UI

--- src/UI/screen_Learning/createmodel_screen.py
+++ src/UI/screen_Learning/createmodel_screen.py
@@ -1,9 +1,10 @@
 # Copyright (C) 2021 Andrii Sonsiadlo
 import os
 import re
+from tkinter import filedialog
 import shutil
-from random import random, randint
-
+from dataclasses import dataclass
+import tkinter as tk
 from kivy.clock import mainthread
 from kivy.uix.screenmanager import Screen
 
@@ -16,88 +17,122 @@ from algorithm.InceptionV3 import InceptionV3
 from config import CustomizationConfig, LearningConfig
 
 
-class LearningCreate(Screen):
+@dataclass
+class LearningStates:
+    WAITING = 0x0
+    READY = 0x01
+    IN_PROGRESS = 0x10
+    COMPLETED = 0x11
 
+
+class LearningCreate(Screen):
     algorithms = {
         InceptionV3.name: InceptionV3,
     }
 
+    learning_state = LearningStates.WAITING
+
     def __init__(self, **kw):
         super().__init__(**kw)
-        self.isLearning = False
+        self.selected_video_paths = []
+        # self.action_list = ActionList()
 
     @mainthread
     def clear_inputs(self):
-        if self.isLearning:
-            self.ids.begin_learning_button.color = CustomizationConfig.normal_text_color
-            self.ids.begin_learning_button.text = CustomizationConfig.text_learning
-        else:
-            self.enable_input(False)
-            self.ids.create_model_name.text = ''
-            self.ids.create_author.text = ''
-            self.ids.create_comment.text = ''
+        if self.learning_state == LearningStates.WAITING:
+            self.selected_video_paths = []
 
-            self.new_model = Model()
+            self.enable_epoch_input(False, self.ids.epochs_number)
 
-            self.ids.begin_learning_button.disabled = False
-            self.ids.begin_learning_button.opacity = .8
-            self.ids.begin_learning_button.text = CustomizationConfig.text_train_model
+            # clear inputs
+            self.ids.model_name.text = ''
+            self.ids.author.text = ''
+            self.ids.comment.text = ''
+
+            self.on_spinner_select_algorithm(self.ids.spinner_algorithm.text)
+            self.ids.features_number.text = ''
+            self.ids.max_seq_length.text = ''
+            self.ids.epochs_number.text = ''
+            self.ids.batch_size.text = ''
+            self.ids.max_video_frames.text = ''
+            self.ids.frame_size_w.text = ''
+            self.ids.frame_size_h.text = ''
+
+            if len(self.selected_video_paths) > 0:
+                self.ids.import_videos_loaded.text = str(len(self.selected_video_paths)) + " loaded"
+                self.ids.import_videos_loaded.opacity = 1
+                self.ids.dir_icon.opacity = 0
+                self.set_train_model_btn(LearningStates.READY)
+            else:
+                self.ids.import_videos_loaded.opacity = 0
+                self.ids.dir_icon.opacity = 1
+                self.set_train_model_btn(LearningStates.WAITING)
+
+    def set_train_model_btn(self, state: int):
+        if state == LearningStates.WAITING:
+            self.ids.import_videos_btn.disabled = False
+            self.ids.begin_learning_btn.text = CustomizationConfig.text_train_model
+            self.ids.begin_learning_btn.disabled = True
+            self.ids.begin_learning_btn.opacity = 0
             self.ids.learning_results.opacity = 0
+        elif state == LearningStates.READY:
+            self.ids.import_videos_btn.disabled = False
+            self.ids.begin_learning_btn.disabled = False
+            self.ids.begin_learning_btn.opacity = 1
+            self.ids.begin_learning_btn.text = CustomizationConfig.text_train_model
+            self.ids.begin_learning_btn.color = CustomizationConfig.normal_text_color
+        elif state == LearningStates.IN_PROGRESS:
+            self.ids.import_videos_btn.disabled = True
+            self.ids.begin_learning_btn.text = CustomizationConfig.text_learning
+            self.ids.begin_learning_btn.color = CustomizationConfig.normal_text_color
+        elif state == LearningStates.COMPLETED:
+            self.ids.begin_learning_btn.text = CustomizationConfig.text_completed
+            self.show_results(learning_time=self.new_model.learning_time, threshold=self.new_model.threshold,
+                              accuracy=self.new_model.accuracy)
+            self.ids.begin_learning_btn.disabled = True
+            self.ids.begin_learning_btn.opacity = .5
 
-    def search_person(self, text_filter):
-        search_person_list = []
-        self.ids.rv_box.select_node(None)
-        for person in self.person_list.get_list_with_photo():
-            try:
-                if re.search(str(text_filter).lower(), person.name.lower()):
-                    search_person_list.append(person)
-            except Exception:
-                pass
-        self.set_search_data_recycleview(search_person_list)
+    @mainthread
+    def load_videos(self):
+        root = tk.Tk()
+        root.withdraw()
+        video_paths = filedialog.askopenfilenames(filetypes=[("Video files", ".avi .mp4")])
+        self.selected_video_paths = list(
+            filter(lambda paths: (path if not os.path.split(path)[-1].startswith("._") else ... for path in paths),
+                   video_paths))
+        num_loaded = len(self.selected_video_paths)
 
-    def change_text(self):
-        if self.ids.begin_learning_button.text == CustomizationConfig.text_train_model:
-            self.ids.begin_learning_button.color = CustomizationConfig.normal_text_color
-            self.ids.begin_learning_button.text = CustomizationConfig.text_learning
+        if num_loaded > 0:
+            self.ids.import_videos_loaded.text = str(num_loaded) + " loaded"
+            self.ids.import_videos_loaded.opacity = 1
+            self.ids.dir_icon.opacity = 0
+        self.get_root_window().raise_window()
 
-    def enable_learning_btn(self):  # enables training button
-
-        self.isLearning = False
-        self.ids.begin_learning_button.disabled = False
-        self.ids.begin_learning_button.text = CustomizationConfig.text_train_model
-        self.ids.begin_learning_button.color = CustomizationConfig.normal_text_color
-        self.ids.learning_results.opacity = 0
+        self.set_train_model_btn(LearningStates.READY)
 
     def begin_learning(self):
-        if not self.isLearning:
-            self.isLearning = True
-
-            self.begin_learning_release()
-            # threading.Thread(target=self.begin_learning_release, daemon=True).start()
-
-    def begin_learning_release(self):
         person_list = PersonList()
         model_list = ModelList()
 
-        if self.ids.begin_learning_button.text == CustomizationConfig.text_learning:
+        if self.ids.begin_learning_btn.text == CustomizationConfig.text_learning:
             if len(person_list.get_list_with_photo()):
-                self.new_model.name = self.ids.create_model_name.text
-                self.new_model.author = self.ids.create_author.text
-                self.new_model.comment = self.ids.create_comment.text
+                self.new_model.name = self.ids.model_name.text
+                self.new_model.author = self.ids.author.text
+                self.new_model.comment = self.ids.comment.text
 
                 if self.new_model.name == "":
                     self.new_model.name = CustomizationConfig.text_unnamed
-                    self.ids.create_model_name.text = self.new_model.name
+                    self.ids.model_name.text = self.new_model.name
                 if self.new_model.author == "":
                     self.new_model.author = CustomizationConfig.text_unknown
-                    self.ids.create_author.text = self.new_model.author
+                    self.ids.author.text = self.new_model.author
                 if model_list.check_name_exists(self.new_model.name):
                     print("File", self.new_model.name, "already exists")
                     repeated = 1
                     while model_list.check_name_exists(self.new_model.name + "(" + str(repeated) + ")"):
                         repeated += 1
                     self.new_model.name += "(" + str(repeated) + ")"
-                    self.ids.create_model_name.text = self.new_model.name
+                    self.ids.model_name.text = self.new_model.name
 
                 self.new_model.path_model_data = os.path.join(LearningConfig.folder_models_data, self.new_model.name)
                 if not os.path.isdir(LearningConfig.folder_models_data):
@@ -122,18 +157,12 @@ class LearningCreate(Screen):
 
                     model_list.add_model(self.new_model)
                     model_list.set_selected(model_list.get_list()[-1].name)
-                    self.ids.begin_learning_button.text = CustomizationConfig.text_completed
-
-                    self.show_results(learning_time=self.new_model.learning_time, threshold=self.new_model.threshold,
-                                      accuracy=self.new_model.accuracy)
-
-                    self.ids.begin_learning_button.disabled = True
-                    self.ids.begin_learning_button.opacity = .5
+                    self.set_train_model_btn(LearningStates.COMPLETED)
                     self.new_model = Model()
                 else:
                     self.show_popup_warm(title=title_warn)
-                    self.ids.begin_learning_button.text = CustomizationConfig.text_train_model
-                    self.ids.begin_learning_button.color = CustomizationConfig.normal_text_color
+                    self.ids.begin_learning_btn.text = CustomizationConfig.text_train_model
+                    self.ids.begin_learning_btn.color = CustomizationConfig.normal_text_color
                     try:
                         shutil.rmtree(f"{self.new_model.path_model_data}")
                     except BaseException:
@@ -151,11 +180,6 @@ class LearningCreate(Screen):
             self.ids.learning_results.text = f"Learning time: {learning_time} s, accuracy: {accuracy}"
         self.ids.learning_results.opacity = 1
 
-    def save_model(self):
-        if self.ids.begin_learning_button.text == 'OK':
-            self.ids.begin_learning_button.text = CustomizationConfig.text_train_model
-            self.manager.current = "learning"
-
     @mainthread
     def show_plot(self, data_path):
         plot_path = os.path.join(data_path, 'plot.png')
@@ -166,63 +190,32 @@ class LearningCreate(Screen):
         popupWindow = MyPopupWarn(text=title)
         popupWindow.open()
 
-    def enable_input(self, value):
-        if value is True:
-            self.ids.neighbor_checkbox.active = True
-            self.ids.create_neighbor_num.disabled = False
-            self.ids.create_neighbor_num.text = ''
-            self.ids.create_neighbor_num.hint_text = 'auto'
+    def enable_epoch_input(self, active, inputfield):
+        if active:
+            inputfield.disabled = False
+            inputfield.text = ''
+            inputfield.hint_text = 'auto'
         else:
-            self.ids.neighbor_checkbox.active = False
-            self.ids.create_neighbor_num.disabled = True
-            self.ids.create_neighbor_num.text = 'auto'
-
-    def cancel_learning(self):
-        print("learning canceled by user")
-
-    def refresh(self):
-        self.on_spinner_select_algorithm(self.ids.spinner_algorithm.text)
-        self.set_data_recycleview()
+            inputfield.disabled = True
+            inputfield.text = 'auto'
 
     def on_spinner_select_algorithm(self, algorithm):
         if algorithm == InceptionV3.name:
             self.ids.inceptionv3_settings_box.opacity = 1
+            self.set_text_weights_spinner()
             # self.ids.svm_algorithm_box.opacity = 0
         elif algorithm == "":
             # self.ids.svm_algorithm_box.opacity = 1
             self.ids.inceptionv3_settings_box.opacity = 0
 
-    def on_spinner_select_weights(self, weights):
-        self.weight_selected = weights
-
-    def on_spinner_select_gamma(self, gamma):
-        self.gamma_selected = gamma
-
     def get_values_algorithm(self):
-        if self.algorithms:
-            return list(self.algorithms.keys())
-        else:
-            return []
-
-    def get_values_weights(self):
-        if LearningConfig.weights_values:
-            return LearningConfig.weights_values
-        else:
-            return []
-
-    def get_values_gamma(self):
-        if LearningConfig.gamma_values:
-            return LearningConfig.gamma_values
-        else:
-            return []
+        return list(self.algorithms.keys()) if self.algorithms else []
 
     def set_text_algorithm_spinner(self):
         return self.get_values_algorithm()[0]
 
-    def set_text_weights_spinner(self):
-        self.weight_selected = LearningConfig.weights_values[0]
-        return self.weight_selected
+    def get_values_weights(self):
+        return LearningConfig.weights_values if LearningConfig.weights_values else []
 
-    def set_text_gamma_spinner(self):
-        self.gamma_selected = LearningConfig.gamma_values[0]
-        return self.gamma_selected
+    def set_text_weights_spinner(self):
+        return self.get_values_weights()[0]
